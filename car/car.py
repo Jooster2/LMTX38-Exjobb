cc3fedb8309168e66752ce716378f312873cea44
Joakim Schmidt <joakim.schmidt@gmail.com>
2016-05-23 14:45:35 +0200

    Major cleanup, new class for grabcar
    Signed-off-by: Joakim Schmidt <joakim.schmidt@gmail.com>

--- car/car.py
+++ car/car.py
@@ -12,35 +12,8 @@ class Car:
     Class that handles the driving of the car and its special 
     capabilities.
     """
-    def __init__(self):
-        self.wh = wifihelper.wifi_helper ()
-        wiringpi.pinMode(3, wiringpi.GPIO.OUTPUT)
-        wiringpi.digitalWrite (3,1)
         
-    def drive(self, speed):
-        """
-            Sets the direction and speed that the car should be driven.
-        This is received from wifi_helper. Max speed is 480 which is forward,
-        -480 is reverse. 
-        """
-        print ("IN DRIVE ", speed) 
-        self.speed =int((float(speed)/100) * MAX_SPEED)
-        print ("IN DRIVE, AFTER CALC", self.speed)
-        motors.motor1.setSpeed(self.speed)
 
-
-    def turn(self,direction):
-        """
-        Turns the car to either left or right
-        Called from wifihelper
-        """
-        if direction == "LEFT":
-            motors.motor2.setSpeed(MAX_SPEED)
-
-        elif direction == "RIGHT" :
-            motors.motor2.setSpeed(-MAX_SPEED)
-        else:
-            motors.motor2.setSpeed(0)
         
     def receive_data (self, msg):
         """
--- mapgenerator/car.py
+++
@@ -1,6 +0,0 @@
-from enum import IntEnum
-
-class Car(IntEnum):
-    BIG = 0
-    CAM = 1
-    GRAB = 2
